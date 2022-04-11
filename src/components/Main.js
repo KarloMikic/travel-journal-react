8cb745b948f8e2dfc6c85753892d7aac933b31fa
Karlo Mikić <karlo.mikic.dev@gmail.com>
2022-04-11 12:36:59 +0200

    norefferer added

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,7 +9,7 @@ export default function Main(props){
         <div className="main">
             <img className="main--img" alt="" src={props.imageUrl} />
             <section className ="main--right">
-                <p className="main--top"><img className ="main--map-icon" alt="" src={mapIcon}/><span className ="main--location">{props.location}</span><a className="grey-link" href={props.googleMapsUrl} target="_blank">View on Google Maps</a></p>
+                <p className="main--top"><img className ="main--map-icon" alt="" src={mapIcon}/><span className ="main--location">{props.location}</span><a className="grey-link" href={props.googleMapsUrl} target="_blank" rel="noreferrer">View on Google Maps</a></p>
                 <h1>{props.title}</h1>
                 <p className="main--date">{props.startDate} - {props.endDate}</p>
                 <p className="main--desc">{props.description}</p>
